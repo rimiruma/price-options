30c838ae25e734b28c9f2f5e392bafbd120053c0
rimiruma <rimiruma12@gmail.com>
2024-10-28 11:30:32 +0600

    added navbar

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import Navbar from './Components/Navbar/Navbar'
+// import DaisyNav from './Components/DaisyNav/DaisyNav'
+
+function App() {
+
+  return (
+    <>
+    {/* <DaisyNav></DaisyNav> */}
+    <Navbar></Navbar>
+      <h1 className='text-7xl bg-orange-500'>Vite + React</h1>
+      
+
+
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import { list } from "postcss";
+import Link from "../Link/Link";
+
+
+const Navbar = () => {
+    const routes = [
+        { id: 1, path: "/", name: "Home" },
+        { id: 2, path: "/about", name: "About Us" },
+        { id: 3, path: "/services", name: "Services" },
+        { id: 4, path: "/contact", name: "Contact" },
+        { id: 5, path: "/profile/:id", name: "Profile" }
+      ];
+      
+      
+    return (
+        <nav>
+            <ul className="md:flex">
+            {
+                routes.map(route => <Link key={route.id} route={route}></Link>)
+            }
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
